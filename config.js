7666243c3a39717ffc071b89575b7e9d1d18ea9a
samnguyen539 <trongnguyen81@gmail.com>
2026-08-28 10:39:03 +0700

    feat: add runtime Google Drive configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.SAM_TEACHING_CONFIG = Object.freeze({
+  googleClientId: "",
+  driveFolderName: "SamTeachingVisual",
+  driveScope: "https://www.googleapis.com/auth/drive.file",
+  uploadChunkSize: 8 * 1024 * 1024,
+  recordingTimesliceMs: 5000,
+  appVersion: "1.0.0"
+});
